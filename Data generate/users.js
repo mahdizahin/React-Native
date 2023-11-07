fb849102ead90114c5331eb2ef5110f725997280
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-11-07 21:49:13 +0600

    Add files via upload

---
+++ Data generate/users.js
@@ -0,0 +1,42 @@
+export const USERS =[
+    
+    {
+        user: 'samia2',
+        image:'https://scontent.fdac14-1.fna.fbcdn.net/v/t39.30808-6/378339264_122093797202051039_6922568449927874215_n.jpg?stp=c0.106.960.960a_dst-jpg_s851x315&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=sY-TRxIETB8AX-3X_97&_nc_ht=scontent.fdac14-1.fna&oh=00_AfAz_DP1JIkyQelc3zIXSRwMwNY1zk61fq1soxFT3nLfJA&oe=654F3527'
+    },
+    {
+        user: 'mahdizahin',
+        image : 'https://avatars.githubusercontent.com/u/103143342?v=4'
+    },
+   
+    {
+        user: 'samia',
+        image:'https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQh3nAWna3MV4J-reFBiBqtjqH7stH2HqOrRJUGWu3qlAHlXEeNrfW56WiQt85C73Ayopv8qjeAhjQkS6M'
+    },
+   
+    {
+        user: 'samia2',
+        image:'https://media.cnn.com/api/v1/images/stellar/prod/221127164140-lionel-messi-inter-miami-rumors-spt-intl.jpg?c=original'
+    },
+    {
+        user:'Erdugan',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7ilBrvklA9q2P_rsRND_3ZKq_Sx23RHB9lg&usqp=CAU'
+    },
+    {
+        user:'Hitler',
+        image:'https://cdn.britannica.com/58/129958-050-C3FE2DD2/Adolf-Hitler-1933.jpg'
+    },
+    {
+        user:'mbs',
+        image:'https://upload.wikimedia.org/wikipedia/commons/e/e3/Mohammad_bin_Salman_October_2019_%28cropped%29.jpg'
+    },
+    {
+        user:'Erdugan',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7ilBrvklA9q2P_rsRND_3ZKq_Sx23RHB9lg&usqp=CAU'
+    }
+
+
+
+
+
+]
